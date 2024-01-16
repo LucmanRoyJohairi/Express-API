be16d34d6f0249e556f8988eaf79745e6c3b4bdf
Roy Johairi Lucman <lucman.royjohairi@gmail.com>
2024-01-16 20:53:23 +0800

    chore: added eslint config for custom aliasing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "standard-with-typescript",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true // Include Node.js environment here if needed
+  },
+  "settings": {
+    "import/resolver": {
+      "typescript": {}, // This is required for TypeScript path resolution
+      "alias": {
+        "map": [
+          ["@", "./src"],
+          ["@controllers", "./src/controllers"],
+          ["@models", "./src/models"],
+          ["@middlewares", "./src/middlewares"],
+          ["@routes", "./src/routes"],
+          ["@utils", "./src/utils"]
+          // Add more aliases as needed
+        ],
+        "extensions": [".ts", ".js", ".jsx", ".json"]
+      }
     }
-}
+  },
+  "extends": "standard-with-typescript",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    // Add TypeScript parser options if needed
+  },
+  "rules": {
+    // Add ESLint rules if needed
+  }
+};
